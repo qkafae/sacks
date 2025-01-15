ffa921e8497effb83b93932e8024f558ff4e44ee
kafae <104088216+qkafae@users.noreply.github.com>
2025-01-15 09:31:51 +0800

    Create damage.kt

---
+++ src/main/kotlin/me/kafae/sacks/functions/damage.kt
@@ -0,0 +1,19 @@
+package me.kafae.sacks.functions
+
+import org.bukkit.Sound
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
+
+fun damageEntity(e: LivingEntity, n: Double) {
+    var h: Double = e.health
+    h = (h - n).coerceAtLeast(0.0)
+    e.world.playSouns(e.location, Sound.ENTITY_PLAYER_HURT, 1.0f, 1.0f)
+    e.health = h
+}
+
+fun damagePlayer(e: Player, n: Double) {
+    var h: Double = e.health
+    h = (h - n).coerceAtLeast(0.0)
+    e.world.playSouns(e.location, Sound.ENTITY_PLAYER_HURT, 1.0f, 1.0f)
+    e.health = h
+}
